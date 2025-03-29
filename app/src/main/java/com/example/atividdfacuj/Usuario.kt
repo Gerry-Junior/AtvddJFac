404b638d2a74640608f1c4fb97536d7a76c2d7a1
Gerry-Junior <gerry.casilla17@gmail.com>
2025-03-29 18:59:09 -0300

    novAtt

--- app/src/main/java/com/example/atividdfacuj/Usuario.kt
+++ app/src/main/java/com/example/atividdfacuj/Usuario.kt
@@ -8,28 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class IniciarAtividades : AppCompatActivity() {
+class Usuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_iniciar_atividades)
+        setContentView(R.layout.activity_usuario)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-    fun abrirImc(view: View) {
-            val entradaImc = Intent(this, TarefaImc::class.java)
-            startActivity(entradaImc)
-        }
-    fun abrirMediaNota(view: View){
-        val entradaMediAluno = Intent(this, CalcularNotaAluno::class.java)
-        startActivity(entradaMediAluno)
-
-    }
-    fun abrirUsuario(view: View){
-        val entradaUsuario = Intent(this, Usuario::class.java)
-        startActivity(entradaUsuario)
+    fun voltarInicio(view: View) {
+        val inicio = Intent(this, IniciarAtividades::class.java)
+        startActivity(inicio)
     }
 }
